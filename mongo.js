d2805e683a7bc7911b311a15067eb568696c1fb7
sbframee <104187401+sbframee@users.noreply.github.com>
2024-08-17 17:17:29 +0530

    first commit

--- mongo.js
+++ mongo.js
@@ -1,7 +1,8 @@
 const mongoose=require("mongoose")
+const DATABASE = process.env.DATABASE
 
 const connectDB = async () => {
-    const conn = await mongoose.connect("mongodb+srv://sanjuahuja:cY7NtMKm8M10MbUs@cluster0.wdfsd.mongodb.net/MISSK", {
+    const conn = await mongoose.connect( `${DATABASE}`, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
